a6c6c8b0911ce617adcf2ad948ebc17f10e573ac
Jason Zubiate <jzubiate.dev@gmail.com>
2025-04-23 01:17:59 -0700

    video updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,10 +19,10 @@ export default function Home() {
   const y = useTransform(scrollYProgress, [0, 1], ["-15vh", "15vh"]);
 
   return (
-    <main className="pt-52 p-6">
-      <div className="grid grid-cols-12 gap-8 mb-12">
+    <main className="pt-52 p-6 flex flex-col-reverse lg:flex-col gap-12">
+      <div className="flex flex-col-reverse lg:grid lg:grid-cols-12 gap-8">
         <div className="col-span-3">
-          <div className="overflow-hidden mb-2">
+          <div className="overflow-hidden mb-1 lg:mb-2">
             <motion.h2
               initial={{ y: "100%" }}
               animate={{ y: "0%" }}
@@ -57,7 +57,7 @@ export default function Home() {
         </div>
 
         <div className="col-span-6">
-          <div className="overflow-hidden mb-2">
+          <div className="overflow-hidden mb-1 lg:mb-2">
             <motion.h2
               initial={{ y: "100%" }}
               animate={{ y: "0%" }}
@@ -72,7 +72,7 @@ export default function Home() {
             {currentProject.summary.map((line, index) => (
               <div key={index} className="overflow-hidden">
                 <motion.div
-                  className="text-xl font-medium tracking-tight leading-[1.3]"
+                  className="text-[clamp(16px,1.5vw,24px)] font-medium tracking-tight leading-[1.3]"
                   initial={{ y: "100%" }}
                   animate={{ y: "0%" }}
                   transition={{
@@ -90,7 +90,7 @@ export default function Home() {
         </div>
 
         <div className="col-span-3">
-          <div className="overflow-hidden mb-2">
+          <div className="overflow-hidden mb-1 lg:mb-2">
             <motion.h2
               initial={{ y: "100%" }}
               animate={{ y: "0%" }}
@@ -115,7 +115,7 @@ export default function Home() {
       </div>
 
       <div className="space-y-4">
-        <div className="overflow-hidden mb-2">
+        <div className="overflow-hidden mb-1 lg:mb-2">
           <motion.h2
             initial={{ y: "100%" }}
             animate={{ y: "0%" }}
@@ -131,10 +131,10 @@ export default function Home() {
           initial={{ opacity: 0, scale: 0.97, y: 12 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{ duration: 0.3, delay: 0.3, ease: "easeOut" }}
-          className="w-full h-[800px] bg-neutral-900 rounded-xl overflow-hidden relative"
+          className="w-full h-[300px] lg:h-[800px] bg-neutral-900 rounded-lg lg:rounded-xl overflow-hidden relative"
         >
           <motion.div
-            className="absolute inset-0 w-full h-[120%] -top-[20%]"
+            className="absolute inset-0 w-full h-[120%] -top-[10%] lg:-top-[20%]"
             style={{ y }}
           >
             <Image
@@ -149,7 +149,7 @@ export default function Home() {
 
           <video
             src={currentProject.videoUrl}
-            className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[75%] max-w-[1200px] h-auto object-cover z-20 aspect-[auto_16/9] rounded-lg"
+            className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[75%] max-w-[1200px] h-auto object-cover z-20 aspect-[auto_16/9] rounded-md lg:rounded-lg"
             autoPlay
             muted
             loop
