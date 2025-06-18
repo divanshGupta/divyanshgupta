a497c0693a553ddf83f6a4a21527e12b4a6d83ee
Jason Zubiate <jzubiate.dev@gmail.com>
2025-06-17 18:15:20 -0700

    fixes loader bug

--- src/components/layout/LoadingScreen.tsx
+++ src/components/layout/LoadingScreen.tsx
@@ -33,7 +33,7 @@ export default function LoadingScreen() {
         <motion.p
           animate={!isLoading ? { y: "-100%" } : {}}
           transition={{ duration: 0.5, delay: 0.5, ease: [0.7, 0, 0.84, 0] }}
-          className="text-xs lg:text-sm font-semibold tracking-tight"
+          className="text-sm font-semibold tracking-tight"
         >
           {percent}
         </motion.p>
